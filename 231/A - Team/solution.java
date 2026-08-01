7de3ae71ab89852c42662e878c2a2e66a3d2da01
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-08-01 21:13:42 +0530

    Add Team [A] from Codeforces

---
+++ 231/A - Team/solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int count = 0;
+ 
+            for (int i = 0; i < n; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+                int c = sc.nextInt();
+ 
+                if (a + b + c >= 2) {
+                    count++;
+                }
+            }
+ 
+            System.out.println(count);
+        }
+        sc.close();
+    }
+}
