22778ea87fa9c44ebb1524e8e4a6da315b14d20b
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-08-03 20:52:32 +0530

    Add Next Round [A] from Codeforces

---
+++ 158/A - Next Round/solution.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        if (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+ 
+            int[] scores = new int[n];
+            for (int i = 0; i < n; i++) {
+                scores[i] = sc.nextInt();
+            }
+ 
+            int threshold = scores[k - 1];
+            int advancers = 0;
+ 
+            for (int i = 0; i < n; i++) {
+                if (scores[i] >= threshold && scores[i] > 0) {
+                    advancers++;
+                } else {
+                    break;
+                }
+            }
+ 
+            System.out.println(advancers);
+        }
+        sc.close();
+    }
+}
