70a23a685fa8ba396d70a716b721c0271fecfe28
shivaniichaudhary <shivanichaudhary300487@gmail.com>
2026-08-19 22:25:33 +0530

    Add Football [A] from Codeforces

---
+++ 96/A - Football/solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        if (sc.hasNext()) {
+            String s = sc.next();
+ 
+            if (s.contains("0000000") || s.contains("1111111")) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
+    }
+}
